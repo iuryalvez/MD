bd26d4e55aff3eb3ea572d9b819ad5bf5bb55364
Iury Alves <88641647+iuryalvez@users.noreply.github.com>
2022-07-18 18:04:33 -0300

    added new functions
    added print_TCR, aloca_matriz, libera_matriz

--- teoria-dos-numeros/teoria-dos-numeros.h
+++ teoria-dos-numeros/teoria-dos-numeros.h
@@ -18,32 +18,110 @@
 #endif
 */
 
+// função que limpa a tela ("clear"/"cls")
 void clear_screen();
 
+//função que exibe as opções de um 'switch case'
 void menu();
 
-void crivo_de_eratostenes(int n);
+/**
+ * @brief função que calcula o crivo de eratóstenes de um número "n"
+ * 
+ * @param n número referência para o cálculo
+ */
+void crivo_de_eratostenes(long int n);
 
-int num_primo(int n);
+/**
+ * @brief função que informa se o número "n" é primo ou não
+ * 
+ * @param n número n
+ * @return long int, TRUE or FALSE 
+ */
+long int num_primo(long int n);
 
-int mdc_dois_numeros(int n1, int n2);
+/**
+ * @brief função que retorna o maior divisor comum entre dois números
+ * 
+ * @param n1 número 1
+ * @param n2 número 2
+ * @return long int, o maior divisor 
+ */
+long int mdc_dois_numeros(long int n1, long int n2); 
 
-int MDC(int n);
+/**
+ * @brief função que retorna o maior divisor de um número "n"
+ * 
+ * @param n número
+ * @return long int, maior divisor 
+ */
+long int MD(long int n);
 
-void arvore_de_fatores(int n);
+/**
+ * @brief função que exibe a árvore de fatores de um número "n"
+ * 
+ * @param n número "n"
+ */
+void arvore_de_fatores(long int n);
 
-void euclides(int a, int b);
+/**
+ * @brief função que calcula e exibe o algoritmo de euclides
+ * 
+ * @param a número Za
+ * @param b número b
+ */
+void euclides(long int a, long int b);
 
-void euclides_estendido(int a, int b);
+/**
+ * @brief função que calcula e exibe o algoritmo de euclides estendido
+ * 
+ * @param a número Za
+ * @param b número b
+ */
+void euclides_estendido(long int a, long int b);
 
-int maior_numero(int n1, int n2);
+/**
+ * @brief função que retorna o "t" do algoritmo de euclides estendido
+ * 
+ * @param n1 número Zn
+ * @param n2 número "a"
+ * @return long int, o "t"  
+ */
+long int t_euclides_estendido(long int n1, long int n2);
 
-int num_inverso(int n, int t);
+/**
+ * @brief função que retorna o maior número entre número 1 e 2
+ * 
+ * @param n1 número 1
+ * @param n2 número 2
+ * @return long int, maior número 
+ */
+long int maior_numero(long int n1, long int n2);
 
-void resolve_equacoes();
+/**
+ * @brief função que retorna o número inverso de um número "n" em um Za
+ * 
+ * @param n número "n"
+ * @param t número "t" calculado com o algoritmo de euclides estendido
+ * @return long int, número inverso 
+ */
+long int num_inverso(long int n, long int t);
 
+// função que calcula e exibe a solução de uma equação linear modular
+void equacoes_lineares();
+
+// função que calcula e exibe a solução de sistema lineares modulares utilizando o teorema chinês do resto 
 void tcr();
 
+void print_TCR(long int **tabela_tcr, int s, long int x);
+
+long int **aloca_matriz(int tam);
+
+void libera_matriz(long int **matriz, int tam);
+
+// long int verifica_tcr(long int **tabela, long int s, long int x);
+
+// função que pula uma linha no printf
 void new_line();
 
+// função que exibe a mensagem de saída
 void end_msg();
